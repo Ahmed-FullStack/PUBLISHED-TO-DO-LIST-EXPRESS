6ed2ebd415b646e36bb4a41ce6a2632e1a371464
ahmed ashraf <ahmedashraf2568@gmail.com>
2025-01-05 18:19:25 +0500

    string

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/static'));
 
 mongoose.connect(process.env.API);
+console.log(process.env.API);
 
 // mongoose.connect('mongodb://localhost:27017/todolistDB');
 
